38123ec3ac21be35c011c1d6cf8f10f2e5840fdc
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-02-23 15:22:34 -0500

    geolocalisation link to user

--- src/Api/MapBox/Map.component.jsx
+++ src/Api/MapBox/Map.component.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import ReactMapGl from 'react-map-gl';
-
+import ReactMapGl, {Marker} from 'react-map-gl';
+import * as userData from '../../places/pages/UserPlaces'
 
 export default function Map() {
   const [viewport, setViewport] = useState({
@@ -15,12 +15,21 @@ export default function Map() {
     <div>
     <ReactMapGl
       {...viewport}
+      mapStyle="mapbox://styles/devprojetsalex/ck6zdo8vw0fjd1ili5b1pj0pj"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
       onViewportChange={viewport => {
         setViewport(viewport)
       }}
       >
-        
+        {userData.DUMMY_PLACES.map((userLocations) => (
+          <Marker
+          key={userLocations.id}
+          latitude={userLocations.location.lat}
+          longitude={userLocations.location.lng}
+          >
+            <div>&#10060; </div>
+          </Marker>
+        ))}
     </ReactMapGl>  
     </div>
   );
