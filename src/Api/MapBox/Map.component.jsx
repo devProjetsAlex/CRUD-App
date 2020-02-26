b733bfde9f3d69c12b6c1be0f246bba24c621e64
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-02-26 00:23:39 -0500

    save

--- src/Api/MapBox/Map.component.jsx
+++ src/Api/MapBox/Map.component.jsx
@@ -1,21 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ReactMapGl, {Marker} from 'react-map-gl';
 import * as userData from '../../places/pages/UserPlaces'
 
 const Map = () => {
-
+ 
   const [viewport, setViewport] = useState({
     latitude:48.7484405,
     longitude: -73.9878584,
     width: "39.3vw",
     height: "40vh",
     zoom: 3
-
   });
 
   return (
     <div>
-
     <ReactMapGl
       {...viewport}
       mapStyle="mapbox://styles/devprojetsalex/ck6zdo8vw0fjd1ili5b1pj0pj"
@@ -24,21 +22,16 @@ const Map = () => {
         setViewport(viewport)
       }}
       >
-
-
         {userData.DUMMY_PLACES.map((userLocations) => (
           <Marker
           key={userLocations.id}
           latitude={userLocations.location.lat}
           longitude={userLocations.location.lng}
-          >
-
+          >           
           &#10060;
-
           </Marker>
         ))}
     </ReactMapGl>  
-
     </div>
   );
 }
